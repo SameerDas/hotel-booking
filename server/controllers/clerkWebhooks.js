b54cac80b29eecb2f2f343903a1e87790a643251
SameerDas <sameerdas63048@gmail.com>
2025-06-11 12:30:42 +0530

    modified

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -7,7 +7,7 @@ const clerkWebhooks=async()=>{
     const whook=new Webhook(process.env.CLERK_WEBHOOKS_SECRET)
 
     // getting headers
-    const header={
+    const headers={
       "svix-id":req.headers["svix-id"],
       "svix-timestamp":req.headers["svix-timestamp"],
       "svix-signature":req.headers["svix-signature"],
@@ -22,8 +22,8 @@ const clerkWebhooks=async()=>{
     const userData={
       _id:data.id,
       email:data.email_addresses[0].email_address,
-      username:data.first.name + " " + data.last.name,
-      image:data.image.url,
+      username:data.first_name + " " + data.last_name,
+      image:data.image_url,
     }
 
     // Switch Cases for different Events.
